c80d2bf7365e9c153c01ea568bd5dd50172af5d1
Nidhi Thakkar <nthak055@uottawa.ca>
2023-07-02 23:47:01 -0400

    first commit

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+  
+      <section className="home-section">
+        <h1>Canada is more than its hulking-mountain, craggy-coast good looks: it also cooks extraordinary meals, rocks cool culture, and unfurls wild, moose-spotting road trips.</h1>
+        <img src="canada.jpg" alt="Canada Landscape" />
+      </section>
+    
+    </div>
+  );
+}
+
+export default HomePage;
